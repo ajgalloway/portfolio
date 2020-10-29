bbeb75c210a74d17503c242b4b028f158c8bde34
Andrew Galloway <ajgalloway97@gmail.com>
2020-10-29 16:35:51 -0400

    Updates

---
+++ precache-manifest.dddbbecff06269b20d39d96ebc8735bb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501d1f467d2d692a4b66f016b93188c9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "81f47eb9dfd6cec87fd9",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a570a7ba79beaa92104b",
+    "url": "/portfolio/static/css/main.d0e81b23.chunk.css"
+  },
+  {
+    "revision": "81f47eb9dfd6cec87fd9",
+    "url": "/portfolio/static/js/2.eb491344.chunk.js"
+  },
+  {
+    "revision": "4d7ef388ded4c46d8f17e43a588896c3",
+    "url": "/portfolio/static/js/2.eb491344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a570a7ba79beaa92104b",
+    "url": "/portfolio/static/js/main.15f07a6b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a255aaefe2e5e754ab8c2fe2627c42d1",
+    "url": "/portfolio/static/media/360fuel.a255aaef.png"
+  },
+  {
+    "revision": "cebcdb3a6a30459eae00f13316798fd4",
+    "url": "/portfolio/static/media/ag_logo_black.cebcdb3a.png"
+  },
+  {
+    "revision": "de3a399a85fdf8e2f794866dfdb3d336",
+    "url": "/portfolio/static/media/ag_logo_white.de3a399a.png"
+  },
+  {
+    "revision": "7efb722b8e3baf41f94a566c43431e6b",
+    "url": "/portfolio/static/media/free_speech_messenger.7efb722b.jpg"
+  },
+  {
+    "revision": "3e90170fe755f007db14c5b001625e4d",
+    "url": "/portfolio/static/media/gophr.3e90170f.png"
+  },
+  {
+    "revision": "05fca361fd67cd0a98785acb7a33e549",
+    "url": "/portfolio/static/media/headshot.05fca361.png"
+  },
+  {
+    "revision": "a38e2fabd3209d1daf59ff680460f0c7",
+    "url": "/portfolio/static/media/in_store_kiosk.a38e2fab.png"
+  },
+  {
+    "revision": "407ac8ec759dc018a534616cf4eb03c4",
+    "url": "/portfolio/static/media/mcneese.407ac8ec.png"
+  },
+  {
+    "revision": "32ce7ac9ac8ccdcc0c0a444d419d9725",
+    "url": "/portfolio/static/media/professional_website.32ce7ac9.png"
+  },
+  {
+    "revision": "0068e4da1bd82b7f91922b372ddcb12e",
+    "url": "/portfolio/static/media/resume.0068e4da.pdf"
+  }
+]);
